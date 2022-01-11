75a243eac15792cce50783069edc88bdb0b3e6eb
Peter <18681694607@163.com>
2022-01-11 11:05:08 +0800

    2-7 修改配置文件，验证Dao

---
+++ src/main/java/com/peter/o2o/dao/AreaDao.java
@@ -0,0 +1,13 @@
+package com.peter.o2o.dao;
+
+import com.peter.o2o.entity.Area;
+
+import java.util.List;
+
+public interface AreaDao {
+    /**
+     * 列出区域列表
+     * @return areaList
+     */
+    List<Area> queryArea();
+}
