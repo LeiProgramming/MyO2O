ecb6b9730e7fa199653e1ef62e3fa9ab68067043
Peter <18681694607@163.com>
2022-01-12 09:40:07 +0800

    4-3 Thumbnailator图片处理和封装Util

---
+++ src/main/java/com/peter/o2o/util/ImageUtil.java
@@ -0,0 +1,111 @@
+package com.peter.o2o.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+
+import net.coobird.thumbnailator.Thumbnails;
+
+import net.coobird.thumbnailator.geometry.Positions;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import javax.imageio.ImageIO;
+
+public class ImageUtil {
+	private static String basePath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+	private static final SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+	private static final Random r = new Random();
+
+	public static String generateThumbnail(CommonsMultipartFile thumbnail, String targetAddr) {
+		String realFileName = getRandomFileName();
+		String extension = getFileExtension(thumbnail);
+		makeDirPath(targetAddr);
+		String relativeAddr = targetAddr + realFileName + extension;
+		File dest = new File(PathUtil.getImgBasePath() + relativeAddr);
+		try {
+			Thumbnails.of(thumbnail.getInputStream()).size(200, 200)
+					.watermark(Positions.BOTTOM_RIGHT, ImageIO.read(new File(basePath + "watermark.jpg")), 0.25f)
+					.outputQuality(0.8f).toFile(dest);
+		} catch (IOException e) {
+			throw new RuntimeException("创建缩略图失败：" + e.toString());
+		}
+		return relativeAddr;
+	}
+
+	/**
+	 * 生成随机文件名, 当前时间+五位随机数
+	 *
+	 * @return
+	 */
+	private static String getRandomFileName() {
+		int rannum = r.nextInt(89999) + 10000;
+		String nowTimeStr = sDateFormat.format(new Date());
+		return nowTimeStr + rannum;
+	}
+
+	public static String generateNormalImg(CommonsMultipartFile thumbnail, String targetAddr) {
+		String realFileName = ImageUtil.getRandomFileName();
+		String extension = getFileExtension(thumbnail);
+		makeDirPath(targetAddr);
+		String relativeAddr = targetAddr + realFileName + extension;
+		File dest = new File(PathUtil.getImgBasePath() + relativeAddr);
+		try {
+			Thumbnails.of(thumbnail.getInputStream()).size(337, 640).outputQuality(0.5f).toFile(dest);
+		} catch (IOException e) {
+			throw new RuntimeException("创建缩略图失败：" + e.toString());
+		}
+		return relativeAddr;
+	}
+
+	public static List<String> generateNormalImgs(List<CommonsMultipartFile> imgs, String targetAddr) {
+		int count = 0;
+		List<String> relativeAddrList = new ArrayList<String>();
+		if (imgs != null && imgs.size() > 0) {
+			makeDirPath(targetAddr);
+			for (CommonsMultipartFile img : imgs) {
+				String realFileName = ImageUtil.getRandomFileName();
+				String extension = getFileExtension(img);
+				String relativeAddr = targetAddr + realFileName + count + extension;
+				File dest = new File(PathUtil.getImgBasePath() + relativeAddr);
+				count++;
+				try {
+					Thumbnails.of(img.getInputStream()).size(600, 300).outputQuality(0.5f).toFile(dest);
+				} catch (IOException e) {
+					throw new RuntimeException("创建图片失败：" + e.toString());
+				}
+				relativeAddrList.add(relativeAddr);
+			}
+		}
+		return relativeAddrList;
+	}
+
+	/**
+	 * 创建目标路径所涉及到的目录，没有当前目录则创建
+	 *
+	 * @param targetAddr
+	 */
+	private static void makeDirPath(String targetAddr) {
+		String realFileParentPath = PathUtil.getImgBasePath() + targetAddr;
+		File dirPath = new File(realFileParentPath);
+		if (!dirPath.exists()) {
+			dirPath.mkdirs();
+		}
+	}
+
+	/**
+	 * 获取输入文件流的拓展名
+	 *
+	 * @param cFile
+	 * @return
+	 */
+	private static String getFileExtension(CommonsMultipartFile cFile) {
+		String originalFileName = cFile.getOriginalFilename();
+		return originalFileName.substring(originalFileName.lastIndexOf("."));
+	}
+}
